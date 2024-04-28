797e67a9b4ea9b57e6982012471c2423c4fa4068
Nguyễn Thái Hòa <hoa41300@gmail.com>
2024-04-29 06:58:34 +0700

    Update VoiceRecorder class to record audio with dynamic length.

--- KaraokeGameProject/Assets/_Scripts/Audio/VoiceRecorder.cs
+++ KaraokeGameProject/Assets/_Scripts/Audio/VoiceRecorder.cs
@@ -1,6 +1,3 @@
-using System;
-using System.IO;
-using System.Text;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -9,33 +6,47 @@ public class VoiceRecorder : MonoBehaviour
     [SerializeField] private string fileName;
 
     private static AudioSource audioSource;
-    private static float[] samplesData;
-
+    private bool isRecording;
     private string currentMicrophone = string.Empty;
-    private int defaultSampleRate = 44100;
+    private float beginRecordTime;
 
-    const string WAV_FILE_EXTENSION = ".wav";
-    const int HEADER_SIZE = 44;
+    const int DEFAULT_SAMPLE_RATES = 44100;
+    const int MAX_RECORDING_DURATION_SECONDS = 300;
+    const int MAX_SAMPLES_COUNT = DEFAULT_SAMPLE_RATES * MAX_RECORDING_DURATION_SECONDS;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        isRecording = false;
         InitializeMicrophone();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            ListMicrophones();
-        }
-        if(Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.I))
         {
             StartRecording();
         }
         if (Input.GetKeyDown(KeyCode.O))
         {
             StopRecording();
+        }
+        AutomaticallyStopRecording();
+    }
+
+    /// <summary>
+    /// Automatically stop and save the voice recording when the audio clip reach maximum length.
+    /// </summary>
+    private void AutomaticallyStopRecording()
+    {
+        // Check if only is recording player voice
+        if (isRecording)
+        {
+            if (Microphone.GetPosition(currentMicrophone) >= MAX_SAMPLES_COUNT)
+            {
+                Debug.Log("Recording has exceeds maximum length. Stop recording and save recorded data!");
+                StopRecording();
+            }
         }
     }
 
@@ -50,13 +61,14 @@ public class VoiceRecorder : MonoBehaviour
         currentMicrophone = Microphone.devices[0];
     }
 
-    private void ListMicrophones()
+    [ContextMenu("List Available Microphones")]
+    public void ListMicrophones()
     {
         for (int i = 0; i < Microphone.devices.Length; i++)
         {
             Debug.Log("Microphone #" + (i + 1) + ": " + Microphone.devices[i]);
             Microphone.GetDeviceCaps(Microphone.devices[i], out int minFrequency, out int maxFrequency);
-            Debug.Log($"Min freq: {minFrequency} - Max freq: {maxFrequency}");
+            Debug.Log($"Min frequency: {minFrequency} - Max frequency: {maxFrequency}");
         }
     }
 
@@ -68,7 +80,10 @@ public class VoiceRecorder : MonoBehaviour
             return;
         }
         Debug.Log("Start recording!");
-        audioSource.clip = Microphone.Start(currentMicrophone, true, 5, defaultSampleRate);
+        beginRecordTime = Time.time;
+        isRecording = true;
+        Debug.Log("Begin recording at: " + beginRecordTime);
+        audioSource.clip = Microphone.Start(currentMicrophone, true, MAX_RECORDING_DURATION_SECONDS, DEFAULT_SAMPLE_RATES);
     }
 
     public void StopRecording()
@@ -78,10 +93,18 @@ public class VoiceRecorder : MonoBehaviour
             Debug.Log("Failed to stop recording. Current microphone is not available!");
             return;
         }
+        isRecording = false;
         Debug.Log("Start saving file!");
-        SaveRecording();
+        SavingWavFile();
         Debug.Log("Stop recording!");
         Microphone.End(currentMicrophone);
+    }
+
+    public string SavingWavFile()
+    {
+        string filePath;
+        WavUtility.FromAudioClip(ExtractRecordedSound(audioSource.clip), out filePath, Application.streamingAssetsPath, fileName);
+        return filePath;
     }
 
     private bool IsCurrentMicrophoneAvailable()
@@ -93,135 +116,36 @@ public class VoiceRecorder : MonoBehaviour
         return true;
     }
 
-    private void SaveRecording()
+    /// <summary>
+    /// Extracts a portion of the original audio clip based on the elapsed recording time.
+    /// </summary>
+    /// <param name="originalClip">The original audio clip to extract from.</param>
+    /// <returns>A new AudioClip containing the recorded sound.</returns>
+    private AudioClip ExtractRecordedSound(AudioClip originalClip)
     {
-        // Wait until the microphone starts recording,
-        // ensures that the subsequent code (such as retrieving audio data) runs only after recording has begun
-        while (!(Microphone.GetPosition(null) > 0)) { }
-
-        samplesData = new float[audioSource.clip.samples * audioSource.clip.channels];
-        audioSource.clip.GetData(samplesData, 0);
-        string filePath = Path.Combine(Application.streamingAssetsPath, fileName + WAV_FILE_EXTENSION);
-
-        // Delete the file if already exists
-        if (File.Exists(filePath))
+        // Check if the input clip is null
+        if (originalClip == null)
         {
-            File.Delete(filePath);
+            Debug.LogError("Input clip is null. Cannot extract recorded sound.");
+            return null;
         }
+        // Calculate the time elapsed since recording started
+        float timeSinceRecordStarted = Time.time - beginRecordTime;
+        // Calculate the samples per second (sampling rate)
+        float samplesPerSec = originalClip.samples / originalClip.length;
 
-        try
-        {
-            WriteWAVFile(audioSource.clip, filePath);
-            Debug.Log("File Saved Successfully at: " + filePath);
-        }
-        catch (DirectoryNotFoundException)
-        {
-            Debug.LogError("Please, Create a StreamingAssets Directory in the Assets Folder");
-        }
-        catch (Exception e)
-        {
-            //check for other Exceptions
-            Debug.Log(e.Message);
-        }
-    }
+        // Determine the number of recorded samples based on elapsed time
+        int recordedSampleCount = Mathf.FloorToInt(samplesPerSec * timeSinceRecordStarted);
 
-    private static void WriteWAVFile(AudioClip clip, string filePath)
-    {
-        float[] clipData = new float[clip.samples];
+        // Create an array to store the recorded audio samples
+        float[] recordedSamples = new float[recordedSampleCount];
+        originalClip.GetData(recordedSamples, 0);
 
-        // Create the file
-        using (Stream fs = File.Create(filePath))
-        {
-            int frequency = clip.frequency;
-            int numOfChannels = clip.channels;
-            int samples = clip.samples;
+        // Create a new AudioClip to hold the recorded sound
+        AudioClip recordedClip = AudioClip.Create("RecordedSound", recordedSampleCount, 1, DEFAULT_SAMPLE_RATES, false);
+        recordedClip.SetData(recordedSamples, 0);
 
-            // Move the file pointer to the beginning of a file
-            fs.Seek(0, SeekOrigin.Begin);
-
-            //Header
-
-            // Chunk ID
-            byte[] riff = Encoding.ASCII.GetBytes("RIFF");
-            fs.Write(riff, 0, 4);
-
-            // ChunkSize
-            byte[] chunkSize = BitConverter.GetBytes((HEADER_SIZE + clipData.Length) - 8);
-            fs.Write(chunkSize, 0, 4);
-
-            // Format
-            byte[] wave = Encoding.ASCII.GetBytes("WAVE");
-            fs.Write(wave, 0, 4);
-
-            // Subchunk1ID
-            byte[] fmt = Encoding.ASCII.GetBytes("fmt ");
-            fs.Write(fmt, 0, 4);
-
-            // Subchunk1Size
-            byte[] subChunk1 = BitConverter.GetBytes(16);
-            fs.Write(subChunk1, 0, 4);
-
-            // AudioFormat
-            byte[] audioFormat = BitConverter.GetBytes(1);
-            fs.Write(audioFormat, 0, 2);
-
-            // NumChannels
-            byte[] numChannels = BitConverter.GetBytes(numOfChannels);
-            fs.Write(numChannels, 0, 2);
-
-            // SampleRate
-            byte[] sampleRate = BitConverter.GetBytes(frequency);
-            fs.Write(sampleRate, 0, 4);
-
-            // ByteRate
-            byte[] byteRate = BitConverter.GetBytes(frequency * numOfChannels * 2); // sampleRate * bytesPerSample*number of channels, here 44100*2*2
-            fs.Write(byteRate, 0, 4);
-
-            // BlockAlign
-            ushort blockAlign = (ushort)(numOfChannels * 2);
-            fs.Write(BitConverter.GetBytes(blockAlign), 0, 2);
-
-            // BitsPerSample
-            ushort bps = 16;
-            byte[] bitsPerSample = BitConverter.GetBytes(bps);
-            fs.Write(bitsPerSample, 0, 2);
-
-            // Subchunk2ID
-            byte[] datastring = Encoding.ASCII.GetBytes("data");
-            fs.Write(datastring, 0, 4);
-
-            // Subchunk2Size
-            byte[] subChunk2 = BitConverter.GetBytes(samples * numOfChannels * 2);
-            fs.Write(subChunk2, 0, 4);
-
-            // Data
-
-            clip.GetData(clipData, 0);
-            short[] intData = new short[clipData.Length];
-            byte[] bytesData = new byte[clipData.Length * 2];
-
-            int convertionFactor = 32767;
-
-            for (int i = 0; i < clipData.Length; i++)
-            {
-                intData[i] = (short)(clipData[i] * convertionFactor);
-                byte[] byteArr = new byte[2];
-                byteArr = BitConverter.GetBytes(intData[i]);
-                byteArr.CopyTo(bytesData, i * 2);
-            }
-
-            fs.Write(bytesData, 0, bytesData.Length);
-        }
-    }
-
-    public static byte[] ConvertWAVtoByteArray(string filePath)
-    {
-        //Open the stream and read it back.
-        byte[] bytes = new byte[audioSource.clip.samples + HEADER_SIZE];
-        using (FileStream fs = File.OpenRead(filePath))
-        {
-            fs.Read(bytes, 0, bytes.Length);
-        }
-        return bytes;
+        // Return the newly created recorded clip
+        return recordedClip;
     }
 }
